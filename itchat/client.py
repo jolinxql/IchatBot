744a25f8f268a6aacc12d3cc71a7a4758a58ef81
Ting Pan (MSR Student-Person Consulting) <v-tinpan@microsoft.com>
2016-07-31 15:22:59 +0800

    chg

--- itchat/client.py
+++ itchat/client.py
@@ -491,8 +491,8 @@ class client(object):
         if toUserName is None: toUserName = self.storageClass.userName
         mediaId = self.__upload_file(fileDir, isPicture = not fileDir[-4:] == '.gif')
         if mediaId is None or mediaId == '':
-            self.send_msg(u'[暂不支持官方表情]', toUserName)
-            return False
+            self.send(u'[暂不支持官方表情]', toUserName)
+            return True
         url = '%s/webwxsendmsgimg?fun=async&f=json'%self.loginInfo['url']
         payloads = {
             'BaseRequest': self.loginInfo['BaseRequest'],
